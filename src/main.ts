aeb8f2c3c438a2380d45dbe4ae2b7f1c3d6dde84
darisjerez <darisjerez@gmail.com>
2020-09-26 14:11:28 -0400

    add cors and rate limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,POST",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
+    app.use(
+      rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+      }),
+    );
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
